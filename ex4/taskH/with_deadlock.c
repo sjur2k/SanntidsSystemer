4ef7aaf19f94db22751218b2b24800c980c89cf3
Martin Repvik Olsbø <martinolsbo1@gmail.com>
2026-09-24 13:36:42 +0200

    ex4 finished

--- ex4/taskH/with_deadlock.c
+++ ex4/taskH/with_deadlock.c
@@ -6,32 +6,36 @@
 pthread_t philosophers[N];
 pthread_mutex_t forks[N];
 
-int philosopher(void* arg){
-    int self_idx = (int)(long)arg;
-    int left_fork_idx = (self_idx + N - 1) % N;
-    int right_fork_idx = (self_idx + 1) % N;
-    while (1){
-        pthread_mutex_lock(&forks[left_fork_idx]);
-        usleep(10000); // Helps induce deadlock.
-        pthread_mutex_lock(&forks[right_fork_idx]);
-        printf("Philosopher %d eating\n", self_idx);
-
-        pthread_mutex_unlock(&forks[left_fork_idx]);
-        pthread_mutex_unlock(&forks[right_fork_idx]);
-        printf("Philosopher %d thinking\n", self_idx);
-    }
-}
+void* philosopher(void* arg);
 
 int main(){
     
     for(int i = 0; i < N; i++){
-        pthread_create(&philosophers[i],NULL,(void*)philosopher,(void*)(long)i);
         pthread_mutex_init(&forks[i],NULL);
     }
+    for(int i = 0; i < N; i++){
+        pthread_create(&philosophers[i],NULL,philosopher,(void*)(long)i);
+    }    
 
     for(int i = 0; i < N; i++){
         pthread_join(philosophers[i], NULL);
         pthread_mutex_destroy(&forks[i]);
     }
     return 0;
+}
+
+void* philosopher(void* arg){
+    int self_idx = (int)(long)arg;
+    int left_fork_idx = (self_idx + N - 1) % N;
+    int right_fork_idx = (self_idx + 1) % N;
+    while (1){
+        pthread_mutex_lock(&forks[left_fork_idx]);
+        usleep(1000);
+        printf("Philosopher %d has one fork\n", self_idx);
+        pthread_mutex_lock(&forks[right_fork_idx]);
+        printf("Philosopher %d has both forks\n", self_idx);
+
+        pthread_mutex_unlock(&forks[left_fork_idx]);
+        pthread_mutex_unlock(&forks[right_fork_idx]);
+    }
 }
--- ex4/taskH/without_deadlock.c
+++ ex4/taskH/without_deadlock.c
@@ -1,71 +1,107 @@
 #include <pthread.h>
+#include <semaphore.h>
 #include <unistd.h>
 #include <stdio.h>
 
 #define N 5 // Number of forks and philosophers
+int colors[N] = {91, 92, 93, 94, 96};
+typedef enum {
+    THINKING,
+    HUNGRY,
+    EATING,
+} PhilosopherState;
+
+pthread_mutex_t critical_section_mtx;
+sem_t both_forks[N];
 pthread_t philosophers[N];
-pthread_mutexattr_t fork_attrs[N];
-pthread_mutex_t forks[N];
-pthread_barrier_t barrier;
+PhilosopherState states[N];
 
 void* philosopher(void* arg);
 
-void create_and_start_task(pthread_t *thread, void *function, int policy, int index)
-{
-	int ret = 0;
-	pthread_attr_t tattr;
-	struct sched_param param;
-	ret += pthread_attr_init(&tattr);
-	ret += pthread_attr_getschedparam(&tattr, &param);
-	ret += pthread_attr_setschedpolicy(&tattr, policy);
-	ret += pthread_attr_setinheritsched(&tattr, PTHREAD_EXPLICIT_SCHED);
-	param.sched_priority = index + 1;
-	ret += pthread_attr_setschedparam(&tattr, &param);
-	ret += pthread_create(thread, &tattr, function, (void*)(long)index);
-	if (ret != 0)
-	{
-		printf("Error during task init!");
-	}
-}
+void think(int idx);
+void take_forks(int idx);
+void eat(int idx);
+void put_forks(int idx);
+int left(int idx);
+int right(int idx);
 
-
-int main(){
-
-    pthread_barrier_init(&barrier, NULL, N + 1); // N philosophers + 1 main thread
-
-    int policy = SCHED_RR;
+int main(){   
+    pthread_mutex_init(&critical_section_mtx, NULL);
     for(int i = 0; i < N; i++){
-        pthread_mutexattr_init(&fork_attrs[i]);
-	    pthread_mutexattr_setprotocol(&fork_attrs[i], PTHREAD_PRIO_INHERIT);
-        pthread_mutex_init(&forks[i],&fork_attrs[i]);
-        
-        create_and_start_task(&philosophers[i],(void*)philosopher, policy, i);
+        sem_init(&both_forks[i], 0, 0);
     }
-    usleep(100 * 1000);
-    pthread_barrier_wait(&barrier);
+    for(int i = 0; i < N; i++){
+        pthread_create(&philosophers[i],NULL,philosopher,(void*)(long)i);
+    }
     
     for(int i = 0; i < N; i++){
         pthread_join(philosophers[i], NULL);
-        pthread_mutex_destroy(&forks[i]);
     }
-    pthread_barrier_destroy(&barrier);
+    for(int i = 0; i < N; i++){
+        sem_destroy(&both_forks[i]);
+    }
+    pthread_mutex_destroy(&critical_section_mtx);
     return 0;
 }
 
 void* philosopher(void* arg){
     int self_idx = (int)(long)arg;
-    int left_fork_idx  = (self_idx + N - 1) % N;
-    int right_fork_idx = (self_idx + 1) % N;
-    pthread_barrier_wait(&barrier);
-    while (1){
-        pthread_mutex_lock(&forks[left_fork_idx]);
-        usleep(10000); // Helps induce deadlock.
-        pthread_mutex_lock(&forks[right_fork_idx]);
-        printf("Philosopher %d eating\n", self_idx);
 
-        pthread_mutex_unlock(&forks[left_fork_idx]);
-        pthread_mutex_unlock(&forks[right_fork_idx]);
-        printf("Philosopher %d thinking\n", self_idx);
+    while (1){
+        think(self_idx);
+        take_forks(self_idx);
+        eat(self_idx);
+        put_forks(self_idx);
     }
 }
 
+void think(int idx){
+    printf("Philosopher %d is thinking\n",idx + 1);
+    usleep(100 * 1000);
+}
+
+// Must only be called within a critical section.
+void try_forks(int idx){
+    if (states[idx] == HUNGRY &&
+        states[left(idx)] != EATING &&
+        states[right(idx)] != EATING)
+    {
+        states[idx] = EATING;
+        sem_post(&both_forks[idx]);
+    }
+}
+
+void take_forks(int idx){
+    pthread_mutex_lock(&critical_section_mtx);
+    {
+        states[idx] = HUNGRY;
+        printf("Philosopher %d is hungry\n", idx + 1);
+        try_forks(idx); // Check if forks are available.
+    }
+    pthread_mutex_unlock(&critical_section_mtx);
+
+    sem_wait(&both_forks[idx]);
+}
+
+void eat(int idx){
+    usleep(200 * 1000);
+    printf("\033[1;%dmPhilosopher %d is eating\033[0;0m\n", colors[idx],idx + 1);
+}
+
+void put_forks(int idx){
+    pthread_mutex_lock(&critical_section_mtx);
+    {
+        states[idx] = THINKING;
+        try_forks(left(idx)); //Tries to release neighbor fork semaphores.
+        try_forks(right(idx));
+    }
+    pthread_mutex_unlock(&critical_section_mtx);
+}
+
+int left(int idx){
+    return (idx + N - 1) % N;
+}
+
+int right(int idx){
+    return (idx + 1) % N;
+}
